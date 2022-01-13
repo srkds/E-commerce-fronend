5dd921f9151af01bf6a2c2b4a5e9e6bd723eccce
srkds <im.nirav86@gmail.com>
2022-01-13 13:27:15 +0530

    add: admin dashboard ui

--- src/user/AdminDashBoard.js
+++ src/user/AdminDashBoard.js
@@ -1,10 +1,71 @@
 import React from "react";
 import Base from "../core/Base";
+import { isAuthenticated } from "../auth/helper";
+import { Link } from "react-router-dom";
 
 const AdminDashboard = () => {
+  const {
+    user: { name, email, role },
+  } = isAuthenticated();
+
+  const adminLeftSide = () => {
+    return (
+      <div className="card">
+        <h4 className="card-header bg-dark text-white">Admin Navigation</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <Link className="nav-link text-success" to="/admin/create/category">
+              Create Categories
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link text-success" to="/admin/create/product">
+              Create Product
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link text-success" to="/admin/products">
+              Manage Products
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link text-success" to="/admin/orders">
+              Manage Order
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  const adminRightSide = () => {
+    return (
+      <div className="card mb-4">
+        <h4 className="card-header">Admin Information</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <span className="badge bg-success mr-2">Name:</span> {name}
+          </li>
+          <li className="list-group-item">
+            <span className="badge bg-success mr-2">email:</span> {email}
+          </li>
+          <li className="list-group-item">
+            <span className="badge bg-danger mr-2">Admin Area</span>
+          </li>
+        </ul>
+      </div>
+    );
+  };
   return (
-    <Base title="Admin Dashboard Page">
-      <h1>This is Admin Dashboard page</h1>
+    <Base
+      title="Welcome To Admin Dashboard"
+      description="Manage your products here."
+      className="container bg-success p-4"
+    >
+      <div className="row">
+        <div className="col-3">{adminLeftSide()}</div>
+        <div className="col-9">{adminRightSide()}</div>
+      </div>
     </Base>
   );
 };
