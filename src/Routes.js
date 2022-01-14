39e06a936ef47724a76e7168a3d62d81f82de20b
srkds <im.nirav86@gmail.com>
2022-01-14 20:22:46 +0530

    add: update product inside admin dashboard

--- src/Routes.js
+++ src/Routes.js
@@ -11,6 +11,7 @@ import AddCategory from "./admin/AddCategory";
 import ManageCategories from "./admin/ManageCategories";
 import AddProduct from "./admin/AddProduct";
 import ManageProducts from "./admin/ManageProducts";
+import UpdateProduct from "./admin/UpdateProduct";
 
 const MyRoutes = () => {
   return (
@@ -34,6 +35,11 @@ const MyRoutes = () => {
         />
         <AdminRoute path="/admin/create/product" exact component={AddProduct} />
         <AdminRoute path="/admin/products" exact component={ManageProducts} />
+        <AdminRoute
+          path="/admin/product/update/:productId"
+          exact
+          component={UpdateProduct}
+        />
       </Switch>
     </BrowserRouter>
   );
