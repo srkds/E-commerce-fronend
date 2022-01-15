7898e5a4cc32dfa58cb9c1392fbe9a3305c325ad
srkds <im.nirav86@gmail.com>
2022-01-15 09:57:10 +0530

    add: reusable card, all products load to home

---
+++ src/core/helper/imageHelper.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { API } from "../../backend";
+
+const ImageHelper = ({ product }) => {
+  const imageUrl = product
+    ? `${API}/product/photo/${product._id}`
+    : "https://images.pexels.com/photos/1092423/pexels-photo-1092423.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+  //   console.log(product);
+
+  return (
+    <div className="rounded border border-success p-2">
+      <img
+        src={imageUrl}
+        alt="photo"
+        style={{ maxHeight: "100%", maxWidth: "100%" }}
+        className="mb-3 rounded"
+      />
+    </div>
+  );
+};
+
+export default ImageHelper;
